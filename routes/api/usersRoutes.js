a21772d75efbd201fd12a8c0737771bc0e0f68b0
EsTrevino <sttrevino2@gmail.com>
2018-05-03 09:55:16 -0700

    added User schema

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-// @route GET 'api/users/test'
-//@desc 'tests post route'
+//@route:  GET 'api/users/test'
+//@desc: 'tests post route'
 //@access: public route
 router.get('/test', (req, res) =>{
   res.json({message: "User route works"});
