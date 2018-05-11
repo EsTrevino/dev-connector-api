44e48ba8c6b90cda025905f1c57dfb4a6faca7a4
EsTrevino <sttrevino2@gmail.com>
2018-05-11 16:49:56 -0700

    added token authentication without signingin logic

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 import history from "../history";
+import store from "../store";
 
 import Navbar from "./layout/Navbar";
 import Footer from "./layout/Footer";
@@ -9,6 +11,18 @@ import Register from "./auth/Register";
 import Login from "./auth/Login";
 import Test from "./test";
 import "../style/app.css";
+import setAuthToken from "../utils/SetAuthToken";
+import { AUTH_USER } from "../actions/types";
+
+if (localStorage.token) {
+  let token = localStorage.getItem("token");
+  setAuthToken(token);
+  let decoded = jwt_decode(token);
+  store.dispatch({
+    type: AUTH_USER,
+    payload: decoded
+  });
+}
 
 class App extends Component {
   render() {
